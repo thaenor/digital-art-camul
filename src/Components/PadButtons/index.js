05185428b5ea98b869a1ebee424fbd3c444bd787
Francisco Santos <1100584@isep.ipp.pt>
2019-05-25 20:35:44 +0100

    initial note recording logic

--- src/Components/PadButtons/index.js
+++ src/Components/PadButtons/index.js
@@ -1,56 +1,131 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './styles.css';
 
-export default function PadButton() {
-  return (
-    <div>
-      <table>
-        <tbody>
-        <tr>
-          <th className="square">
-            <button className="c3 container">Dó</button>
-          </th>
-          <th className="square">
-            <button className="csharp3 container">Dó#</button>
-          </th>
-          <th className="square">
-            <button className="d3 container">Ré</button>
-          </th>
-          <th className="square">
-            <button className="dsharp3 container">Ré#</button>
-          </th>
-        </tr>
-        <tr>
-          <th className="square">
-            <button className="e3 container">Mi</button>
-          </th>
-          <th className="square">
-            <button className="f3 container">Fá</button>
-          </th>
-          <th className="square">
-            <button className="fsharp3 container">Fá#</button>
-          </th>
-          <th className="square">
-            <button className="g3 container">Sol</button>
-          </th>
-        </tr>
-        <tr>
-          <th className="square">
-            <button className="gsharp3 container">Sol#</button>
-          </th>
-          <th className="square">
-            <button className="a3 container">Lá</button>
-          </th>
-          <th className="square">
-            <button className="asharp3 container">Lá#</button>
-          </th>
-          <th className="square">
-            <button className="b3 container">Si</button>
-          </th>
-        </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+export default class PadButtons extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onNoteClick(note) {
+    this.props.onSavedNote(note);
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Dó')}
+                  className="c3 container"
+                >
+                  Dó
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Dó#')}
+                  className="csharp3 container"
+                >
+                  Dó#
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Ré')}
+                  className="d3 container"
+                >
+                  Ré
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Ré#')}
+                  className="dsharp3 container"
+                >
+                  Ré#
+                </button>
+              </th>
+            </tr>
+            <tr>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Mi')}
+                  className="e3 container"
+                >
+                  Mi
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Fá')}
+                  className="f3 container"
+                >
+                  Fá
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Fá#')}
+                  className="fsharp3 container"
+                >
+                  Fá#
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Sol')}
+                  className="g3 container"
+                >
+                  Sol
+                </button>
+              </th>
+            </tr>
+            <tr>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Sol#')}
+                  className="gsharp3 container"
+                >
+                  Sol#
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Lá')}
+                  className="a3 container"
+                >
+                  Lá
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Lá#')}
+                  className="asharp3 container"
+                >
+                  Lá#
+                </button>
+              </th>
+              <th className="square">
+                <button
+                  onClick={this.onNoteClick.bind(this, 'Si')}
+                  className="b3 container"
+                >
+                  Si
+                </button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+PadButtons.propTypes = {
+  onSavedNote: PropTypes.func
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PadButtons from "./Components/PadButtons";
-import NoteQueue from "./Components/NoteQueue";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PadButtons from './Components/PadButtons';
+import NoteQueue from './Components/NoteQueue';
 
-import "./styles.css";
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PadButtons />
-      <NoteQueue />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    }
+  }
+
+  onSavedNote(notePlayed) {
+    console.log(notePlayed);
+    //let newNoteQueue = this.state.notes;
+    //newNoteQueue.push(notePlayed);
+    //this.setState((state) => ({ notes: newNoteQueue}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PadButtons onSavedNote={this.onSavedNote}/>
+        <NoteQueue />
+      </div>
+    );
+  }
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
